cb88ba0c557f11b9b336c73f6e63b8309b2339e1
Gustavo_Lima.37 <129420909+gustavolima37@users.noreply.github.com>
2023-08-11 13:17:20 -0300

    att

---
+++ curso.em.video/testes/index.js
@@ -0,0 +1,13 @@
+//Forma de enviar dados ao node
+
+let readline = require('readline-sync')
+
+//Pedindo dados ao terminal
+
+
+//Verificando quem é o maior e menor valor entre: valor1, valor2 ou valor3
+
+
+
+//Resposta digitar no terminal: node e o nome do arquivo. (no meu caso testes)
+console.log(`O menor valor digitado foi ${menor} e o maior valor foi ${maior}`)
